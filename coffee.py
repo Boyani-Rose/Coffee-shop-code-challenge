a73afc62486d7bacf6422dab24b8b3314d98af0e
Rose Momanyi <rosebmomanyi@gmail.com>
2025-05-17 15:13:10 +0300

    Added name property and validation to the coffee class

--- coffee.py
+++ coffee.py
@@ -1,3 +1,15 @@
 
 class Coffee:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) >= 3:
+            self._name = value
+        else:
+            raise ValueError("Coffee name must be a string with at least 3 characters.")
