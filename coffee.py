c71eebac6c4b9a4b6eb75036af03a8f89f164063
Rose Momanyi <rosebmomanyi@gmail.com>
2025-05-17 15:46:03 +0300

    Added num_orders and average_price methods to Customer class

--- coffee.py
+++ coffee.py
@@ -19,3 +19,12 @@ class Coffee:
 
     def customers(self):
         return list(set(order.customer for order in self.orders()))
+    
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if orders:
+            return sum(order.price for order in orders) / len(orders)
+        return 0
